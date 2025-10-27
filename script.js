ab741b63c06f83167f7a5f37d18114ebb87bf4a5
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-10-27 07:40:44 +0100

    zmienne, console log i prompt

---
+++ script.js
@@ -0,0 +1,24 @@
+// do zmiennych można przypisać wartości
+
+// const lub let - tworzymy zmienne
+const imie = "Jan" // string
+
+// nie można tego zrobić:
+// imie = "Janek"
+
+// stworzenie zmiennej
+let wiek // undefined
+
+// przypisanie wartości do zmiennej
+wiek = 15 // number
+
+let waga = 67.8 // number
+
+console.log(imie)
+console.log("jakiś mój tekst wpisany ręcznie")
+console.log(wiek)
+console.log("Waga to: " + waga)
+
+
+prompt("Wpisz liczbę")
+console.log()
